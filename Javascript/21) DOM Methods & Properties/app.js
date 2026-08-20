6bb0a902f9074283d7fd733c5d7c14a472323a01
alihuzaifa-stackone <ali.huzaifa@stackone.com>
2026-08-20 18:56:46 +0500

    dom methods and auth screen screen has covered for lms

---
+++ Javascript/21) DOM Methods & Properties/app.js
@@ -0,0 +1,12 @@
+// console.log(document.childNodes[1].childNodes[2].childNodes[1].innerHTML = "Hello World");
+
+
+// nodeType
+// console.log(document.nodeType);
+// // var headingEl = document.querySelector('h1');
+// var paraEl = document.querySelector('p');
+// console.log(paraEl.childNodes[1].nodeType);
+
+// console.log(document.childNodes[0].nextSibling);
+console.log(document.childNodes[1].previousSibling);
+
