32c68140baa3b94ed7a0369b3c0af3a1101c4862
Ali Huzaifa <104046450+alihuzaifa@users.noreply.github.com>
2026-09-10 18:46:23 +0500

    add lesson 22 async await and lesson 23 ecommerce listing and details section integration

---
+++ Javascript/22) Async Await/app.js
@@ -0,0 +1,24 @@
+// const getData = async () => {
+//     try {
+//         const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+//         const convertedData = await data.json()
+//         console.log(convertedData);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// getData()
+
+
+// const init = async () => {
+
+// }
+
+
+// async function init() {
+//     return "Hello"
+// }
+
+// const data = init();
+// console.log(data);
